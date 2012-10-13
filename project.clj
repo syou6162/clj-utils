e93aa5819b5086badd8ec5aa3707d3d9a09e1e51
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-13 21:19:40 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject info.yasuhisay/clj-utils "0.1.0-SNAPSHOT"
+  :description "My utils for clojure"
+  :url "https://github.com/syou6162/clj-utils"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
