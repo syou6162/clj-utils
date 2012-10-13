9ec3477ffe0589819b27db4313cb4628b2ac86a1
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-13 21:48:21 +0900

    temporary fileを扱えるようなマクロを追加

--- src/clj_utils/io.clj
+++ src/clj_utils/io.clj
@@ -1,4 +1,5 @@
-(ns clj-utils.io)
+(ns clj-utils.io
+  (:require [clojure.java.io :as io]))
 
 (import '(java.io File FileReader))
 
@@ -9,3 +10,12 @@
 (defn deserialize [filename]
   (with-open [inp (-> (File. filename) java.io.FileInputStream. java.io.ObjectInputStream.)]
     (.readObject inp)))
+
+(defmacro with-temp-file
+  [[varname & [content]] & body]
+  `(let [~varname (java.io.File/createTempFile "yasuhisay", ".tmp")]
+     (when-let [content# ~content]
+       (io/copy content# ~varname))
+     (let [rv# (do ~@body)]
+       (.delete ~varname)
+       rv#)))
