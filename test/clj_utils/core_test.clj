2e7920f49965d19bd48ac0bed088ec2d581854d8
Yasuhisa Yoshida <syou6162@gmail.com>
2012-11-04 04:15:36 +0900

    added some tests

--- test/clj_utils/core_test.clj
+++ test/clj_utils/core_test.clj
@@ -1,6 +1,17 @@
 (ns clj-utils.core-test
   (:use clojure.test
-        clj-utils.core))
+        clj-utils.core)
+  (:use [clojure.string :only (split)]))
+
+(deftest flip-test
+  (let [s "a b c"]
+    (is (= (split s #"\s")
+           (flip split #"\s" s)))))
+
+(deftest hash-map-by-names-test
+  (is (= (let [x :x, y :y]
+           (hash-map-by-names [x y]))
+         {:y :y, :x :x})))
 
 (deftest split-with-ratio-test
   (is (= (split-with-ratio 0.9 (range 10))
