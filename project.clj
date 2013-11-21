7ca9f8453ee30b097d9cc018bafe20ef31e29781
Yasuhisa Yoshida <syou6162@gmail.com>
2013-11-21 21:02:25 +0900

    versionを0.1.2に上げた

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject info.yasuhisay/clj-utils "0.1.1"
+(defproject info.yasuhisay/clj-utils "0.1.2"
   :description "My utils for clojure"
   :url "https://github.com/syou6162/clj-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/timbre "2.7.1"]])
